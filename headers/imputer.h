140c4916d3f97cda2dd909779009c54ffe5f6317
Yarno Grenier <127676205+yarno97x@users.noreply.github.com>
2025-12-29 20:41:43 -0500

    Changed ColumnChange to std::map

--- headers/imputer.h
+++ headers/imputer.h
@@ -3,36 +3,21 @@
 #include <rapidcsv.h>
 #include <algorithm>
 #include <stdexcept>
-
-enum class TRANSFORM 
-{
-    MEAN,
-    MEDIAN,
-    FREQUENCY,
-    CONSTANT,
-    NOT_FITTED
-};
-
-struct ColumnChange 
-{
-    std::string columnName;
-    std::string value;
-};
+#include <queue>
 
 class Imputer {
     public:
         Imputer(rapidcsv::Document& doc);
 
-        void fit_mean(const std::vector<std::string>& columns);
-        void fit_median(const std::vector<std::string>& columns);
-        void fit_frequency(const std::vector<std::string>& columns); 
-        void constant(const std::vector<std::string>& columns, const std::string value);
-        void applyTransform(const rapidcsv::Document& doc);
-        std::vector<ColumnChange> parameters{};
+        void fit_mean(const std::vector<std::string>& columns, bool clear = false);
+        void fit_median(const std::vector<std::string>& columns, bool clear = false);
+        void fit_frequency(const std::vector<std::string>& columns, bool clear = false); 
+        void constant(const std::vector<std::string>& columns, const std::string value, bool clear = false);
+        void applyTransform();
+        std::map<std::string, std::string> parameters{};
 
     private:
         rapidcsv::Document& dataset;
-        TRANSFORM mode = TRANSFORM::NOT_FITTED;
+        bool fitted = false;
         std::vector<std::string> features{};
-
 };
